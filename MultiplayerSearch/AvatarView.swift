6d393ce5292a7ee5c3490e10f1f22305e8421808
Stanislav Kotsar <minolin27@gmail.com>
2019-09-09 00:23:40 +0300

    complete morph animation

--- MultiplayerSearch/AvatarView.swift
+++ MultiplayerSearch/AvatarView.swift
@@ -119,7 +119,25 @@ class AvatarView: UIView {
                             }
         }
             )
+        let morphedFrame = (originalCenter.x > point.x) ?
+            
+            CGRect(x: 0.0, y: bounds.height - morphSize.height,
+                   width: morphSize.width, height: morphSize.height):
+            
+            CGRect(x: bounds.width - morphSize.width,
+                   y: bounds.height - morphSize.height,
+                   width: morphSize.width, height: morphSize.height)
         
+        let morphAnimation = CABasicAnimation(keyPath: "path")
+        morphAnimation.duration = animationDuration
+        morphAnimation.toValue = UIBezierPath(ovalIn:
+            morphedFrame).cgPath
+        
+        morphAnimation.timingFunction = CAMediaTimingFunction(
+            name: .easeOut)
+        
+        circleLayer.add(morphAnimation, forKey: nil)
+        maskLayer.add(morphAnimation, forKey: nil)
     }
   
 }
